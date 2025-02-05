c73a6bea2928e97b4dd20e54c386383a92950f86
Moein Mirkiani <moein.mirkiani@gmail.com>
2025-02-05 17:10:08 +0100

    update flight card display

--- components/Flight/FlightCard.vue
+++ components/Flight/FlightCard.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="p-4 border border-gray-200 rounded-lg hover:bg-gray-50">
-        <h6 class="mb-6">{{ props.airline }}</h6>
+        <h6 class="mb-6">{{ props.flight['smetadata.airlinename'] }}</h6>
+        <p class="mb-4 text-sm text-gray-500">
+            <span>Flight number:</span>
+            <span class="ml-2">{{ props.flight['smetadata.fltnumber'] }}</span>
+        </p>
+        <p class="mb-4">From: {{ props.flight['smetadata.fromdestination'] }}</p>
+        <p class="mb-4">To: {{ props.flight['smetadata.todestination'] }}</p>
+        <Tag :value="props.flight.tname" severity="info" class="capitalize"></Tag>
     </div>
 </template>
 
 <script setup lang="ts">
+import Tag from 'primevue/tag'
+
 interface Props {
-    airline: string
+    flight: {
+        [key: string]: string | null
+    }
 }
 const props = defineProps<Props>()
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,12 @@
     <div class="container mx-auto p-6 border border-gray-200 rounded-lg">
         <h1 class="mb-6 text-2xl">Flights List</h1>
 
-        <div class="space-y-4">
+        <div class="grid grid-cols-12 gap-4">
             <FlightCard
                 v-for="(flight, i) in flights"
                 :key="i"
-                :airline="flight.airline"
+                :flight="flight"
+                class="col-span-6"
             />
         </div>
     </div>
@@ -17,47 +18,54 @@ definePageMeta({
     layout: 'filter'
 })
 
-const flights: { airline: string }[] = [
+const flights: {}[] = [
     {
-        airline: 'Air Canada'
+        "tname": "arrival",
+        "smetadata.fltnumber": "BQ1933",
+        "smetadata.airlinename": "Sky Alps",
+        "smetadata.fromdestination": "LGW",
+        "smetadata.todestination": "BZO",
+        "smetadata.std": "15:40",
+        "smetadata.sta": "18:00",
+        "smetadata.fares": null,
+        "fareType": null
     },
     {
-        airline: 'Air France'
-    },
-    {
-        airline: 'Air India'
-    },
-    {
-        airline: 'Air New Zealand'
-    },
-    {
-        airline: 'Air Serbia'
+        "tname": "departure",
+        "smetadata.fltnumber": "BQ1934",
+        "smetadata.airlinename": "Sky Alps",
+        "smetadata.fromdestination": "BZO",
+        "smetadata.todestination": "LGW",
+        "smetadata.std": "18:40",
+        "smetadata.sta": "21:00",
+        "smetadata.fares": null,
+        "fareType": null
     }
 ]
 
 const route = useRoute()
-const flightsData = ref<{ airline: string }[]>([])
-
-const fetchFlights = async (queryParams: any) => {
-    const { data, error } = await useFetch('http://localhost:8080/flight', {
-        params: queryParams
-    })
-
-    if (error.value) {
-        console.error('Error fetching flights:', error.value)
-    } else {
-        // flightsData.value = data.value
-        console.log(data.value)
-    }
-}
-
-watch(
-    () => route.query,
-    (newQuery) => {
-        fetchFlights(newQuery)
-    },
-    { immediate: true }
-)
-
-await fetchFlights(route.query)
+// const flightsData = ref<{ airline: string }[]>([])
+//
+// const fetchFlights = async (queryParams: any) => {
+//     const { data, error } = await useFetch('http://localhost:8080/flight', {
+//         params: queryParams
+//     })
+//
+//     if (error.value) {
+//         console.error('Error fetching flights:', error.value)
+//     } else {
+//         // flightsData.value = data.value
+//         console.log(data.value)
+//     }
+// }
+//
+// watch(
+//     () => route.query,
+//     (newQuery) => {
+//         fetchFlights(newQuery)
+//     },
+//     { immediate: true }
+// )
+//
+// await fetchFlights(route.query)
 </script>
